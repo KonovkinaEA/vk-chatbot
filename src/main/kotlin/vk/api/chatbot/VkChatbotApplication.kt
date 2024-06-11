2417074718644fe5a9e30e602d2f53c2b0282718
Elizaveta Konovkina <70512444+ElizavetaKkk@users.noreply.github.com>
2024-06-11 14:49:06 +0300

    Initial commit

---
+++ src/main/kotlin/vk/api/chatbot/VkChatbotApplication.kt
@@ -0,0 +1,11 @@
+package vk.api.chatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VkChatbotApplication
+
+fun main(args: Array<String>) {
+	runApplication<VkChatbotApplication>(*args)
+}
---
+++ src/test/kotlin/vk/api/chatbot/VkChatbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package vk.api.chatbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VkChatbotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
